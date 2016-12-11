d8b8502464cb6335cbd0efb20d2a8ca92d74fa9c
Bizongo <bizongo@Bizongos-MacBook-Pro.local>
2016-12-11 18:46:11 +0530

    version change before filter added

--- lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
+++ lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
@@ -6,39 +6,6 @@ module PaperTrail
   # it is possible to sub-class, extend, or even do without this model entirely.
   # See the readme for details.
   class Version < ::ActiveRecord::Base
-    before_create :record_signup
-
-    private
-    def valid_json?(json)
-      begin
-        JSON.parse(json, {:quirks_mode => true})
-        return true
-      rescue => e
-        return false
-      end
-    end
-
-    private
-    def record_signup
-      logg = ::Rails::logger
-      if valid_json?(self.object)
-        data = JSON.parse(self.object, {:quirks_mode => true})
-      else
-        data = self.object.to_s
-      end
-      logg.tagged(:version) do
-        lodata = {
-          "item_id" => self.item_id,
-          "item_type" => self.item_type,
-          "event" => self.event,
-          "whodunnit" => self.whodunnit,
-          "changes" => self.changeset,
-          "object" => data
-        }
-        logg.info(lodata)
-     end
-    end
-
     include PaperTrail::VersionConcern
   end
 end
--- lib/paper_trail/frameworks/rails/controller.rb
+++ lib/paper_trail/frameworks/rails/controller.rb
@@ -32,10 +32,9 @@ module PaperTrail
       # Override this method in your controller to call a different
       # method, e.g. `current_person`, or anything you like.
       def user_for_paper_trail
+        puts "user for paper trail call........................................."
         return unless defined?(current_user)
-        ActiveSupport::VERSION::MAJOR >= 4 ? current_user.try!(:id) : current_user.try(:id)
-      rescue NoMethodError
-        current_user
+        ActiveSupport::VERSION::MAJOR >= 4 ? "#{current_user.class.name}-#{current_user.try!(:id)}-#{current_user.try!(:first_name)}" : "#{current_user.class.name}-#{current_user.try(:id)}-#{current_user.try(:first_name)}"
       end
 
       # Returns any information about the controller or request that you
@@ -80,7 +79,11 @@ module PaperTrail
       # Tells PaperTrail who is responsible for any changes that occur.
       def set_paper_trail_whodunnit
         @set_paper_trail_whodunnit_called = true
-        ::PaperTrail.whodunnit = user_for_paper_trail if ::PaperTrail.enabled_for_controller?
+        ::PaperTrail.whodunnit = user_for_paper_trail
+      end
+
+      def append_info_to_payload(payload)
+        super
       end
 
       # Tells PaperTrail any information from the controller you want to store
@@ -90,23 +93,6 @@ module PaperTrail
       end
 
       def warn_about_not_setting_whodunnit
-        return unless ::PaperTrail.enabled_for_controller?
-
-        user_present = user_for_paper_trail.present?
-        whodunnit_blank = ::PaperTrail.whodunnit.blank?
-        if user_present && whodunnit_blank && !@set_paper_trail_whodunnit_called
-          source_file_location = self.class.instance_methods(false).map { |m|
-            self.class.instance_method(m).source_location.first
-          }.uniq.first
-          ::Kernel.warn <<-EOS.strip_heredoc
-            #{source_file_location}:
-            user_for_paper_trail is present, but whodunnit has not been set.
-            PaperTrail no longer adds the set_paper_trail_whodunnit callback for
-            you. To continue recording whodunnit, please add this before_action
-            callback to your ApplicationController. For more information,
-            please see https://git.io/vrTsk
-          EOS
-        end
       end
     end
   end
